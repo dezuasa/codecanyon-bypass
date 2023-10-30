967845b59ca0ed67a57dd79e740c552b018720ba
dezuasa <145587689+dezuasa@users.noreply.github.com>
2023-10-30 21:10:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const http = require('http');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const { handleRequest } = require('./app');
+const server = http.createServer(handleRequest);
 
 app.get("/", (req, res) => res.send("Ngapa Lo Ajg!"));
 
